02518b0e4ba6862edea16b46eb376c33d0959624
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-22 12:21:53 -0400

    [feature]: add LoginPage

---
+++ src/sections/app/pages/LoginPage/index.tsx
@@ -0,0 +1,45 @@
+import { IconPlugConnected } from '@tabler/icons-react';
+
+import Card from '@/sections/app/components/Card';
+import Button from '@/sections/app/components/Button';
+import TextField from '@/sections/app/components/TextField';
+
+import styles from './LoginPage.module.scss';
+
+const LoginPage = () => {
+  return (
+    <div className={styles.login}>
+      <div className={styles.login__info}>
+        <div className={styles.logo}>
+          <IconPlugConnected size={60} className={styles.logo__icon} />
+          <h1>Gateways</h1>
+        </div>
+        <div className={styles.addorn__one}></div>
+        <div className={styles.addorn__two}></div>
+      </div>
+      <div className={styles.login__section}>
+        <div className={styles.login__card}>
+          <Card>
+            <div className={styles.login__form}>
+              <h1>Login</h1>
+              <form>
+                <TextField
+                  label="Username"
+                  type="text"
+                  placeholder="Introduce your username"
+                />
+                <Button type="submit" variant="primary">
+                  LOGIN
+                </Button>
+              </form>
+            </div>
+          </Card>
+        </div>
+        <div className={styles.circle__one}></div>
+        <div className={styles.circle__two}></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
