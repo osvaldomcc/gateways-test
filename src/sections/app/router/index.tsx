7114f03421f67e93957781a63712d83a90db7fc0
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-23 18:32:40 -0400

    [feature]: add the rest of the routes for peripherals and gateways

--- src/sections/app/router/index.tsx
+++ src/sections/app/router/index.tsx
@@ -1,8 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Outlet, createBrowserRouter } from 'react-router-dom';
+
 import MainLayout from '@/sections/app/layouts/MainLayout';
 import AdminLayout from '@/sections/app/layouts/AdminLayout';
 import LoginPage from '@/sections/app/pages/LoginPage';
 import DashboardPage from '@/sections/app/pages/DashboardPage';
+import GatewayPage from '@/sections/gateway/pages/GatewayPage';
+import PeripheralPage from '@/sections/peripheral/pages/PeripheralPage';
 
 export const router = createBrowserRouter(
   [
@@ -14,13 +17,60 @@ export const router = createBrowserRouter(
           path: 'login',
           element: <LoginPage />,
         },
+
         {
-          path: 'dashboard',
-          element: <DashboardPage />,
-        },
-        {
-          path: 'gateways',
+          path: 'admin',
           element: <AdminLayout />,
+          children: [
+            {
+              index: true,
+              element: <DashboardPage />,
+            },
+            {
+              path: 'gateways',
+              element: <Outlet />,
+              children: [
+                {
+                  index: true,
+                  element: <GatewayPage />,
+                },
+                {
+                  path: 'create',
+                  element: <GatewayPage />,
+                },
+                {
+                  path: ':id/show',
+                  element: <GatewayPage />,
+                },
+                {
+                  path: ':id/edit',
+                  element: <GatewayPage />,
+                },
+              ],
+            },
+            {
+              path: 'peripherals',
+              element: <Outlet />,
+              children: [
+                {
+                  index: true,
+                  element: <PeripheralPage />,
+                },
+                {
+                  path: 'create',
+                  element: <PeripheralPage />,
+                },
+                {
+                  path: ':id/show',
+                  element: <PeripheralPage />,
+                },
+                {
+                  path: ':id/edit',
+                  element: <PeripheralPage />,
+                },
+              ],
+            },
+          ],
         },
       ],
     },
