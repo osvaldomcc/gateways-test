b7efeb838fbd0c0e5ea8accbdff069d8d745c705
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-25 11:54:10 -0400

    [feature]: add GatewayEditPage

---
+++ src/sections/gateway/pages/GatewayEditPage/index.tsx
@@ -0,0 +1,32 @@
+import Card from '@/sections/app/components/Card';
+import GatewayForm, {
+  GatewayFormType,
+} from '@/sections/gateway/components/GatewayForm';
+import styles from './GatewayEditPage.module.scss';
+
+const initialValues: GatewayFormType = {
+  name: 'Cisco',
+  ip: '14.5.21.1',
+};
+
+const GatewayEditPage = () => {
+  const handleSubmit = (some: GatewayFormType) => {
+    console.log(some);
+  };
+  return (
+    <div className={styles.card}>
+      <Card>
+        <div className={styles.card__content}>
+          <h1>Edit Gateway</h1>
+          <GatewayForm
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            isEdit
+          />
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default GatewayEditPage;
