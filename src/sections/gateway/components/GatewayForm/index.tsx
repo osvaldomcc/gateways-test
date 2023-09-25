2fa2060a3b954d716f2146254e54022d5a4df94b
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-25 08:53:48 -0400

    [feature]: add GatewayForm component

---
+++ src/sections/gateway/components/GatewayForm/index.tsx
@@ -0,0 +1,33 @@
+import { Form, Formik } from 'formik';
+
+import TextField from '@/sections/app/components/TextField';
+import Button from '@/sections/app/components/Button';
+import { GatewaySchema } from '@/sections/gateway/utils/GatewaySchema';
+import type { Gateway } from '@/modules/gateway/domain/Gateway';
+import styles from './GatewayForm.module.scss';
+
+export type GatewayFormType = Omit<Gateway, 'id'>;
+
+interface Props {
+  initialValues: GatewayFormType;
+  onSubmit: (gateway: GatewayFormType) => void;
+  isEdit?: boolean;
+}
+
+const GatewayForm = ({ initialValues, onSubmit, isEdit = false }: Props) => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={GatewaySchema}
+      onSubmit={onSubmit}
+    >
+      <Form noValidate className={styles.form}>
+        <TextField placeholder="Cisco..." label="Name" name="name" />
+        <TextField placeholder="120.4.2.10" label="Ip" name="ip" />
+        <Button type="submit">{isEdit ? 'Update' : 'Add'}</Button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default GatewayForm;
