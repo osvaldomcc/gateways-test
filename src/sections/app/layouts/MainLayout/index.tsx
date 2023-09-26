2a8dbcdc9609e3226478ec0712de74abfb574658
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-26 09:23:51 -0400

    [feature]: redirect to last url if logged

--- src/sections/app/layouts/MainLayout/index.tsx
+++ src/sections/app/layouts/MainLayout/index.tsx
@@ -1,19 +1,35 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { useAuthContext } from '@/sections/app/hooks/useAuthContext';
 import { routes } from '@/sections/app/routes';
+import { useEffect } from 'react';
+
 const MainLayout = () => {
   const { user } = useAuthContext();
   const { pathname } = useLocation();
+  const navigate = useNavigate();
+
   const isDefaultOrLogin =
     pathname === routes.login || pathname === routes.default;
 
+  useEffect(() => {
+    if (!isDefaultOrLogin) localStorage.setItem('LAST_URL', pathname);
+  }, [pathname, isDefaultOrLogin]);
+
+  useEffect(() => {
+    if (!user.name) {
+      navigate(routes.login, { replace: true });
+      return;
+    }
+    if (user.name && isDefaultOrLogin) {
+      const LAST_URL = localStorage.getItem('LAST_URL');
+      navigate(LAST_URL ?? routes.dashboard, { replace: true });
+      return;
+    }
+  }, [user, isDefaultOrLogin, navigate]);
+
   return (
     <main>
-      {!user.name && <Navigate to={routes.login} replace />}
-      {user.name && isDefaultOrLogin && (
-        <Navigate to={routes.dashboard} replace />
-      )}
       <Outlet />
     </main>
   );
