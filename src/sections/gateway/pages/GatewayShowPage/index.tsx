3ccea6b63cd49e676603502eb4f1dfc423e895b7
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-26 13:03:57 -0400

    [feature]: add funcionality and improve responsive

--- src/sections/gateway/pages/GatewayShowPage/index.tsx
+++ src/sections/gateway/pages/GatewayShowPage/index.tsx
@@ -1,36 +1,57 @@
+import { useEffect } from 'react';
+
+import { useParams, Link } from 'react-router-dom';
+
 import Card from '@/sections/app/components/Card';
-import { Gateway } from '@/modules/gateway/domain/Gateway';
 import DynamicTable from '@/sections/app/components/DynamicTable';
 import { gatewayColumns } from '@/sections/gateway/types/column';
+import useGateways from '@/sections/gateway/hooks/useGateways';
+import { routes } from '@/sections/app/routes';
 import styles from './GatewayShowPage.module.scss';
 
-const rows: Gateway[] = [
-  {
-    id: 1,
-    name: 'Google',
-    ip: '172.20.12.30',
-  },
-];
-
 const GatewayShowPage = () => {
+  const { id } = useParams();
+  const {
+    gatewayWithPeripherals: gateway,
+    error,
+    isLoading,
+    getGatewayByIdAndPeripherals,
+  } = useGateways();
+
+  useEffect(() => {
+    getGatewayByIdAndPeripherals(Number(id));
+  }, [id]);
+
   return (
     <div className={styles.card}>
       <Card cssStyles={{ flex: 0.8 }}>
         <div className={styles.card__content}>
           <DynamicTable
-            title={rows[0].name}
+            title={gateway?.name}
             columns={gatewayColumns}
-            rows={rows}
+            rows={gateway ? [gateway] : []}
+            isLoading={isLoading}
+            hasError={Boolean(error)}
             hideActions
+            showBackButton
           />
         </div>
       </Card>
       <Card cssStyles={{ flex: 0.2 }}>
         <div className={styles.card__content}>
           <h2>Peripherals:</h2>
-          <h3>Mouse</h3>
-          <h3>Keyboard</h3>
-          <h3>Laptop</h3>
+          <div className={styles.list}>
+            {gateway?.peripherals.map((peripheral) => (
+              <Link
+                className={styles.link}
+                to={routes.peripheralsShow(peripheral.id)}
+                key={peripheral.id}
+              >
+                {peripheral.vendor}
+              </Link>
+            ))}
+          </div>
+          {gateway?.peripherals.length === 0 && <h4>No items to show</h4>}
         </div>
       </Card>
     </div>
