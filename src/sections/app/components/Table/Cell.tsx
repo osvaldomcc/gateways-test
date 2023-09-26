4934622a6e501111d9b90ddf73136be212bf82db
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-26 08:54:00 -0400

    [feture]: add colSpan prop

--- src/sections/app/components/Table/Cell.tsx
+++ src/sections/app/components/Table/Cell.tsx
@@ -4,11 +4,22 @@ interface Props {
   children: ReactNode;
   ariaLabel?: string;
   className?: string;
+  colSpan?: number;
 }
 
-const Cell = ({ children, ariaLabel = 'column', className = '' }: Props) => {
+const Cell = ({
+  children,
+  ariaLabel = 'column',
+  className = '',
+  colSpan = 0,
+}: Props) => {
   return (
-    <td scope="row" aria-label={ariaLabel} className={className}>
+    <td
+      scope="row"
+      aria-label={ariaLabel}
+      className={className}
+      colSpan={colSpan}
+    >
       {children}
     </td>
   );
