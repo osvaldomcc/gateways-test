5cbf4842ed2b1dc92bd29c5082b8cfb3cc57630d
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-26 13:23:23 -0400

    [feature]: add gateways select

--- src/sections/peripheral/components/PeripheralForm/index.tsx
+++ src/sections/peripheral/components/PeripheralForm/index.tsx
@@ -1,34 +1,55 @@
 import { Form, Formik } from 'formik';
+import { useNavigate } from 'react-router-dom';
 
 import TextField from '@/sections/app/components/TextField';
 import Button from '@/sections/app/components/Button';
 import { PeripheralSchema } from '@/sections/peripheral/utils/PeripheralSchema';
 import {
   Status,
-  type Peripheral,
+  type PeripheralBody,
 } from '@/modules/peripheral/domain/Peripheral';
 import styles from './PeripheralForm.module.scss';
 import Select from '@/sections/app/components/Select';
 import { capitalize } from '@/sections/app/utils/capitalize';
+import { Gateway } from '@/modules/gateway/domain/Gateway';
 
-export type PeripheralFormType = Omit<Peripheral, 'id'>;
+export type PeripheralFormType = PeripheralBody;
 
 interface Props {
   initialValues: PeripheralFormType;
   onSubmit: (peripheral: PeripheralFormType) => void;
   isEdit?: boolean;
+  gateways: Gateway[];
 }
 
-const PeripheralForm = ({ initialValues, onSubmit, isEdit = false }: Props) => {
+const PeripheralForm = ({
+  initialValues,
+  onSubmit,
+  isEdit = false,
+  gateways = [],
+}: Props) => {
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate(-1);
+  };
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={PeripheralSchema}
       onSubmit={onSubmit}
+      enableReinitialize
     >
       <Form noValidate className={styles.form}>
         <TextField placeholder="mm/dd/yyyy" label="Date" name="date" />
         <TextField placeholder="Cisco" label="Vendor" name="vendor" />
+        <Select name="gatewayId" label="Gateway">
+          {gateways.map((gateway) => (
+            <option value={gateway.id} key={gateway.id}>
+              {capitalize(gateway.name)}
+            </option>
+          ))}
+        </Select>
         <Select name="status" label="Status">
           {Object.values(Status).map((status) => (
             <option value={status} key={status}>
@@ -36,7 +57,12 @@ const PeripheralForm = ({ initialValues, onSubmit, isEdit = false }: Props) => {
             </option>
           ))}
         </Select>
-        <Button type="submit">{isEdit ? 'Update' : 'Add'}</Button>
+        <div className={styles.buttons}>
+          <Button type="submit">{isEdit ? 'Update' : 'Add'}</Button>
+          <Button type="button" variant="secondary" onPress={handleCancel}>
+            Cancel
+          </Button>
+        </div>
       </Form>
     </Formik>
   );
