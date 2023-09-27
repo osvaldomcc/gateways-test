c54ec59cccd95308431b1b6e763cfd66a1d55f95
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-27 16:30:48 -0400

    [config]: add customRenderHook

--- src/tests/utils/test-utils.tsx
+++ src/tests/utils/test-utils.tsx
@@ -1,13 +1,28 @@
-import { render } from '@testing-library/react';
+import type { ReactNode } from 'react';
+
+import { RenderHookOptions, render, renderHook } from '@testing-library/react';
+
+import AuthProvider from '@/sections/app/contexts/AuthContext';
+
+const wraper = (children: ReactNode) => <AuthProvider>{children}</AuthProvider>;
 
 function customRender(ui: React.ReactElement, options = {}) {
   return render(ui, {
-    // wrap provider(s) here if needed
-    wrapper: ({ children }) => children,
+    wrapper: ({ children }) => wraper(children),
+    ...options,
+  });
+}
+
+function customRenderHook<T, S>(
+  hook: (initialProps: T) => S,
+  options: Omit<RenderHookOptions<T>, 'wrapper'> = {},
+) {
+  return renderHook(hook, {
+    wrapper: ({ children }) => wraper(children),
     ...options,
   });
 }
 
 export { default as userEvent } from '@testing-library/user-event';
 // override render export
-export { customRender as render };
+export { customRender as render, customRenderHook as renderHook };
