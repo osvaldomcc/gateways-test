2b9f443e747fa7c79ed2e55f5e856ed4475aa016
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-28 23:47:18 -0400

    [feature]: add new methods to the factories

--- src/tests/mocks/gateway/gateway-factory.ts
+++ src/tests/mocks/gateway/gateway-factory.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker';
 import { Factory } from 'fishery';
 
 import type { Gateway } from '@/modules/gateway/domain/Gateway';
+import { NAME_MIN_LENGTH } from '@/modules/peripheral/domain/Peripheral';
 
 const GatewayFactory = Factory.define<Gateway>(() => ({
   id: faker.number.int(),
@@ -10,7 +11,20 @@ const GatewayFactory = Factory.define<Gateway>(() => ({
 }));
 
 export const GatewayMother = {
+  create: (params?: Partial<Gateway>): Gateway => {
+    return GatewayFactory.build(params);
+  },
   createList: (length = 5): Gateway[] => {
     return GatewayFactory.buildList(length);
+  },
+  createWithShortName: (): Gateway => {
+    return GatewayFactory.build({
+      name: faker.lorem.word(NAME_MIN_LENGTH - 1),
+    });
+  },
+  createWithInvalidIp: (): Gateway => {
+    return GatewayFactory.build({
+      ip: faker.lorem.word(4),
+    });
   },
 };
--- src/tests/mocks/peripheral/peripheral-factory.ts
+++ src/tests/mocks/peripheral/peripheral-factory.ts
@@ -4,13 +4,17 @@ import { Factory } from 'fishery';
 import { Status } from '@/modules/peripheral/domain/Peripheral';
 import type { Peripheral } from '@/modules/peripheral/domain/Peripheral';
 
-const PeripheralFactory = Factory.define<Peripheral>(() => ({
-  id: faker.number.int(),
-  date: faker.date.anytime().toString(),
-  status: faker.helpers.enumValue(Status),
-  gatewayId: faker.number.int(),
-  vendor: faker.company.name(),
-}));
+const PeripheralFactory = Factory.define<Peripheral>(() => {
+  const date = new Intl.DateTimeFormat('en').format();
+
+  return {
+    id: faker.number.int(),
+    date,
+    status: faker.helpers.enumValue(Status),
+    gatewayId: faker.number.int(),
+    vendor: faker.company.name(),
+  };
+});
 
 export const PeripheralMother = {
   create: (params?: Partial<Peripheral>): Peripheral => {
@@ -18,5 +22,15 @@ export const PeripheralMother = {
   },
   createList: (length = 5): Peripheral[] => {
     return PeripheralFactory.buildList(length);
+  },
+  createWithInvalidDateFormat: (): Peripheral => {
+    return PeripheralFactory.build({
+      date: '1956/24/12',
+    });
+  },
+  createWithInvalidVendor: (): Peripheral => {
+    return PeripheralFactory.build({
+      vendor: `${faker.lorem.word(4)}${faker.number.int(100)}`,
+    });
   },
 };
