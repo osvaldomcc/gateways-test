c9a2050635a82c0a3d406a896148abd0a54bd1d4
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-28 23:47:48 -0400

    [feature]: add handlers for requests

--- src/tests/mocks/gateway/gateway-handlers.ts
+++ src/tests/mocks/gateway/gateway-handlers.ts
@@ -1,15 +1,38 @@
 import { rest } from 'msw';
 
 import { GatewayMother } from './gateway-factory';
-import type { Gateway } from '@/modules/gateway/domain/Gateway';
+import { PeripheralMother } from '@/tests/mocks/peripheral/peripheral-factory';
+import type {
+  Gateway,
+  GatewayWithDependency,
+} from '@/modules/gateway/domain/Gateway';
 
 const GatewayApiUrl = `${import.meta.env.VITE_API_URL}/gateways`;
 
 // Mocked Data
 export const gateways: Gateway[] = GatewayMother.createList(10);
+const gateway = GatewayMother.create();
+const peripherals = PeripheralMother.createList(5);
+
+export const gatewayWithPeripherals: GatewayWithDependency = {
+  ...gateway,
+  peripherals,
+};
 
 export const gatewaysHandlers = [
-  rest.get(GatewayApiUrl, (_, res, ctx) => {
-    return res(ctx.status(200), ctx.json(gateways));
+  rest.get(GatewayApiUrl, (req, res, ctx) => {
+    let result: Gateway[] = gateways;
+    const limit = req.url.searchParams.get('_limit');
+    if (limit) {
+      result = GatewayMother.createList(Number(limit));
+    }
+    return res(ctx.status(200), ctx.json(result));
+  }),
+  rest.get(`${GatewayApiUrl}/:id`, (req, res, ctx) => {
+    const dependency = req.url.searchParams.get('_embed');
+    if (dependency === 'peripherals') {
+      return res(ctx.status(200), ctx.json(gatewayWithPeripherals));
+    }
+    return res(ctx.status(200), ctx.json(GatewayMother.create()));
   }),
 ];
--- src/tests/mocks/peripheral/peripheral-handlers.ts
+++ src/tests/mocks/peripheral/peripheral-handlers.ts
@@ -9,7 +9,15 @@ const PeripheralApiUrl = `${import.meta.env.VITE_API_URL}/peripherals`;
 export const peripherals: Peripheral[] = PeripheralMother.createList(10);
 
 export const peripheralsHandlers = [
-  rest.get(PeripheralApiUrl, (_, res, ctx) => {
-    return res(ctx.status(200), ctx.json(peripherals));
+  rest.get(PeripheralApiUrl, (req, res, ctx) => {
+    let result: Peripheral[] = peripherals;
+    const limit = req.url.searchParams.get('_limit');
+    if (limit) {
+      result = PeripheralMother.createList(Number(limit));
+    }
+    return res(ctx.status(200), ctx.json(result));
+  }),
+  rest.get(`${PeripheralApiUrl}/:id`, (_, res, ctx) => {
+    return res(ctx.status(200), ctx.json(PeripheralMother.create()));
   }),
 ];
