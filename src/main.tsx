fbbf8581094730bdddb04cf99263a0823817496e
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-28 23:44:28 -0400

    [config]: setup msw

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import App from './App.tsx';
 import '@/assets/styles/main.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
+if (import.meta.env.MODE === 'development') {
+  import('./tests/mocks/browser').then(({ worker }) => {
+    // Start the worker.
+    worker.start();
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
