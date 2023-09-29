9928dd65034690a5fae0412abc9a463828857536
Osvaldo Castillo <ocastilloc152@gmail.com>
2023-09-29 09:15:39 -0400

    [remove]: browser mock

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,6 @@ import App from './App.tsx';
 import '@/assets/styles/main.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
-if (import.meta.env.MODE === 'development') {
-  import('./tests/mocks/browser').then(({ worker }) => {
-    // Start the worker.
-    worker.start();
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
